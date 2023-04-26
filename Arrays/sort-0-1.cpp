7572a9fae491ebf61b13479f10db1d91a81e5f75
faiqa1 <faiqarao03@gmail.com>
2023-04-26 21:15:18 +0500

    new commit

--- Arrays/sort-0-1.cpp
+++ Arrays/sort-0-1.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 
 void sortOne(int arr[], int n){
@@ -28,4 +28,37 @@ int main(){
     print(arr,9);
     return 0;
 
+}*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortOne(int arr[], int n){
+    int left = 0, right = n - 1;
+    while (left < right) {
+        if (arr[left] == 1 && arr[right] == 0) {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+        else if (arr[left] == 0) {
+            left++;
+        }
+        else if (arr[right] == 1) {
+            right--;
+        }
+    }
+}
+
+void print(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int arr[] = {1, 1, 0, 1, 1, 0, 0, 0, 0};
+    sortOne(arr, 9);
+    print(arr, 9);
+    return 0;
 }
